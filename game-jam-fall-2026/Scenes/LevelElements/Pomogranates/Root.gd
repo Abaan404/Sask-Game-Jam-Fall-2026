d9ec8db443d23013d8efafcc935a7a7a35ce15d0
abaan404 <67100191+Abaan404@users.noreply.github.com>
2026-09-20 14:32:37 -0600

    bleh

--- game-jam-fall-2026/Scenes/LevelElements/Pomogranates/Root.gd
+++ game-jam-fall-2026/Scenes/LevelElements/Pomogranates/Root.gd
@@ -17,13 +17,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			break
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is not Player:
-		return
-
-	var player: Player = body
-
-	for ability in player.abilities:
-		if ability is RootAbility:
-			for fruit in fruits:
-				fruit.decay_counter = 0.0
-			break
+	pass
