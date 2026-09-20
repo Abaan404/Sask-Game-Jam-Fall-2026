dc944773877d00d7a4c25df1ab13a0dc022730ec
abaan404 <67100191+Abaan404@users.noreply.github.com>
2026-09-20 15:32:17 -0600

    background music

--- game-jam-fall-2026/Scripts/level_button.gd
+++ game-jam-fall-2026/Scripts/level_button.gd
@@ -6,7 +6,7 @@ var split_screen: String = "res://Scenes/HUD/split_screen.tscn"
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 
-
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file(split_screen)
 	SignalHub.current_level = next_scene
+	BackgroundMusic.stream.bus = "Master"
